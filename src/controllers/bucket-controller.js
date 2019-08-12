59642e8ec9886c36311e6b17b278b0247e04a61e
Rich Frost <richard@ipponsolutions.co.uk>
2019-08-12 14:12:05 +0100

    Updated image parsing

--- src/controllers/bucket-controller.js
+++ src/controllers/bucket-controller.js
@@ -97,21 +97,9 @@ const getImage = (request, h) => {
         return Boom.badRequest('Please supply a image name');
     }
     return new Promise(resolve => {
-        minioClient.getObject(bucket, image, (err, dataStream) => {
-            if (err) {
-                return resolve(err);
-            }
-            const data = [];
-            dataStream.on('data', chunk => {
-                data.push(chunk);
-            });
-            dataStream.on('end', () => {
-                resolve(h.response(`data:image/jpeg;base64,${encodeImage(data)}`).header('Content-type', 'image/jpeg'));
-            });
-
-            dataStream.on('error', error => {
-                resolve(error);
-            });
+        minioClient.presignedGetObject(bucket, image, 24 * 60 * 60, (err, presignedUrl) => {
+            if (err) return resolve(err);
+            resolve(presignedUrl);
         });
     });
 };
